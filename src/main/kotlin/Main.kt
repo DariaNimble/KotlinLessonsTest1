d98e1c700570c840edb037ffeda5a06657f6f3a1
DariaNimble <nimblemo@gmail.com>
2025-12-22 14:46:10 +0300

    Update Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,7 +4,7 @@ package org.example
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
     val numberOrders: Int = 75
-    var feedbackText: String = "Cood Order"
+    var feedbackText: String = "Cood Order1"
     println(numberOrders)
     println(feedbackText)
     }

f994d96096b36915b60c696ae2b1045753647677
DariaNimble <nimblemo@gmail.com>
2025-12-23 14:31:52 +0300

    commit3

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,7 +4,14 @@ package org.example
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
     val numberOrders: Int = 75
-    var feedbackText: String = "Cood Order"
+    var feedbackText: String = "Cood Order1"
     println(numberOrders)
     println(feedbackText)
-    }
+   // уволен один var worker: Int = 1999
+   // уволен один  println(worker)
+
+        // уволен второй var worker: Int = 1997
+   // уволен второй println(worker)
+    var worker: Int = 1996
+    println(worker)
+}
